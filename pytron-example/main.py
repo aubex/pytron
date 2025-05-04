fc3678269888f9583345ef8e9fece60db1212c41
Marius Räsener <931919+elmcrest@users.noreply.github.com>
2025-05-04 13:23:07 +0200

    update cargo.toml add example_raise to pytron-example

--- pytron-example/main.py
+++ pytron-example/main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 UV_CACHE_DIR = os.environ.get("UV_CACHE_DIR", False)
+EXAMPLE_RAISE = os.environ.get("EXAMPLE_RAISE", False)
 def main():
     print("Hello from pytron-example!")
     if UV_CACHE_DIR:
@@ -9,6 +10,9 @@ def main():
     
     # Print arguments to show what was passed
     print(f"Arguments received: {sys.argv[1:]}")
+
+    if EXAMPLE_RAISE:
+        raise RuntimeError("I am raised from within main.py")
 
 
 if __name__ == "__main__":
